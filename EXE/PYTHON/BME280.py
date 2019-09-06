0e6a9b76235b38fcec70e33bbce98944890a55d8
mavol15 <36444916+mavol15@users.noreply.github.com>
2019-09-06 20:44:13 +0200

    Update BME280.py
    Changing some bugs in the BME280 driver, now working without problems

--- EXE/PYTHON/BME280.py
+++ EXE/PYTHON/BME280.py
@@ -14,8 +14,7 @@ x = db.cursor()
 sensor = BME280(t_mode=BME280_OSAMPLE_16, p_mode=BME280_OSAMPLE_16, h_mode=BME280_OSAMPLE_16, busnum=2)
 
 temperature = sensor.read_temperature()
-pressure = sensor.read_pressure()
-hectopascals = pascals / 100
+pressure = sensor.read_pressure()/100
 humidity = sensor.read_humidity()
 
 
@@ -24,7 +23,7 @@ humidity = sensor.read_humidity()
 #print 'Humidity  = {0:0.2f} %'.format(humidity)
 
 try:
-    sensor.get_sensor_data()
+    #sensor.get_sensor_data() "Not sure what this should do but the code is working without
     out_to_db = "INSERT INTO sensorDB.sensorTB (sensorTB.time, sensorTB.temp, sensorTB.pres, sensorTB.hum) VALUES (NOW() - INTERVAL MOD((EXTRACT(SECOND FROM NOW())), 10) SECOND,{0:.2f}, {1:.1f}, {2:.2f}) ON DUPLICATE KEY UPDATE sensorTB.temp = {0:.2f}, sensorTB.pres = {1:.1f}, sensorTB.hum = {2:.2f};".format(temperature, pressure, humidity)
     try:
         x.execute(out_to_db)
